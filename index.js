50dae4e70fb2ff1b543875045a9e0825715ebcd9
Aditya Kumar <aditya.95080@gmail.com>
2025-09-25 01:24:29 +0530

    finally added

---
+++ index.js
@@ -0,0 +1,13 @@
+const age=20;
+let name="aditya kumar"
+const db='02/11/2003'
+function sayname(){
+  console.log("aditya kumar");
+}
+let age=45
+if(age>18){
+  console.log("right to vote");
+}
+else{
+  console.log("not voted");
+}
